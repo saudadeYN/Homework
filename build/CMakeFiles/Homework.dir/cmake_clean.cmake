674c8c8f7969e026c1d846d2c2f0187366d410e7
Y.V.Nekrylov <nekrylov.y@gmail.com>
2022-10-23 15:19:54 +0400

    create master

---
+++ build/CMakeFiles/Homework.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework.dir/main.cpp.o"
+  "CMakeFiles/Homework.dir/main.cpp.o.d"
+  "Homework"
+  "Homework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Homework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Homework.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Homework.
+# This may be replaced when dependencies are built.
